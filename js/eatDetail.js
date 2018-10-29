74e9d04f4a722f8b1d7541f949b23d20f6fdd24b
tshoa33 <tshoa33@gmail.com>
2018-10-29 09:32:27 +0800

    hover OK

--- js/eatDetail.js
+++ js/eatDetail.js
@@ -37,38 +37,5 @@ window.onload = function(){
 //加入購物車結束
 
 //新增留言
-window.onload=function(){
-    //create 外殼
-    member_msg = document.createElement('div');  //增加div區塊
-    member_msg.className= 'member_msg clearfix';  //增加區塊後套用原CSS格式
-    document.getElementById('msg').appendChild(member_msg);  //此子層增加div區塊
 
-    var btn = document.getElementsByClassName("comments"); //送出按鈕
-    btn.addEventListener('click',function(){
-        addItem();
-    })
-}
-function addItem(){
-    member_msg = document.createElement('div');
-    member_msg.className= 'member_msg clearfix';
-
-    //create imagearea
-    var member_img = document.createElement('div');
-    member_img.className = 'member_img part-2 part-md-2 part-lg-1';
-    var image = document.createElement('img');
-    image.src = '#';
-    image.alt = '留言者';
-
-    var member_txt = document.createElement('div');
-    member_txt.className= 'member_txt part-10 part-md-10 part-lg-11 clearfix';
-
-    var member_id = document.createElement('div');  //P標籤塞內容
-    member_id.innerHTML = '哆啦A夢';
-
-    document.getElementById('msg').appendChild(member_msg);
-    member_msg.appendChild(member_txt);
-    member_txt.appendChild(member_img);
-    member_txt.appendChild(member_id);
-    member_img.appendChild(image);
-}
 //新增留言結束
